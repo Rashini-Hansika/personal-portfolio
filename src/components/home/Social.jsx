4cbc4c82f485b381cc5d07d26cb5c5e0cbdd0219
Rashini Hansika <88905083+it20639976@users.noreply.github.com>
2023-01-31 13:28:17 +0530

    commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/__rashi_nee/" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/rashini-hansika/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://github.com/it20639976" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
